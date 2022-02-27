baa52dd11786fd87bea7d05ab34181a5875f7ddf
Nikita Avdosev <avdosev1@yandex.ru>
2022-02-27 23:30:28 +0300

    separate text to words

--- lab1/prepare_data.py
+++ lab1/prepare_data.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 data_path = 'data/out/AA'
 
@@ -13,9 +14,23 @@ for file_name in os.listdir(data_path):
             text = json.loads(line)['text']
             texts.append(text)
 
-# todo: 
 # подготовить тексты 
-# разбить на слова 
+# разбить на слова
+uniques_words = set()
+sep = re.compile(r'\W+') 
+for i, text in enumerate(texts):
+    words = re.split(sep, text)
+    words = [s.lower() for s in words if len(s) != 0]
+    # неплохо бы использоваться pymorphy
+    uniques_words.update(words)
+uniques_words = list(uniques_words)
+
+print()
+print('Первый десяток слов')
+print(uniques_words[:10])
+
+
+# todo: 
 # сделать предвычисления
 # сохранить в папку lab1/cahce
 
