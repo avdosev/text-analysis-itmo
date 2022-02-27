224d317c33ae1356e44aba6818ea56e38e371809
Nikita Avdosev <avdosev1@yandex.ru>
2022-02-27 20:57:26 +0300

    add read data

--- lab1/prepare_data.py
+++ lab1/prepare_data.py
@@ -1,1 +1,21 @@
-# nop
+import json
+import os
+
+data_path = 'data/out/AA'
+
+texts = []
+
+for file_name in os.listdir(data_path):
+    print('preparing: {}'.format(file_name))
+    with open(os.path.join(data_path,file_name)) as f:
+        lines = f.readlines()
+        for line in lines:
+            text = json.loads(line)['text']
+            texts.append(text)
+
+# todo: 
+# подготовить тексты 
+# разбить на слова 
+# сделать предвычисления
+# сохранить в папку lab1/cahce
+
