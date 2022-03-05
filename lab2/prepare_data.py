ffda1c82041d7d78be05044a8452f40f235d5486
Nikita Avdosev <avdosev1@yandex.ru>
2022-03-06 00:34:00 +0300

    tokenization

--- lab2/prepare_data.py
+++ lab2/prepare_data.py
@@ -1,6 +1,9 @@
+import json
+import re
+
 filename = 'data/aneks.sql'
 
-
+result = {}
 aneks = []
 with open(filename, encoding='utf-8') as f:
     for line in f:
@@ -15,8 +18,19 @@ with open(filename, encoding='utf-8') as f:
         anek = anek.replace('\\n', '').lower()
 
         aneks.append(anek)
-        print(anek)
 
+tokens = re.compile(r'((\w\.)*\w+)|([,:\-?]|\.+)')
+def use_token(token): 
+    if len(token[0]) == 0: return token[2]
+    else: return token[0] 
+def tokenize(line: str):
+    return [use_token(item) for item in re.findall(tokens, line)]
+
+tokenized_aneks = [tokenize(anek) for anek in aneks[:2]]
+
+result = tokenized_aneks
+with open('lab2/cache/data.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f, indent=2, ensure_ascii=False)
 
 
 
