53b91da410378d22c0bb76cd308cac1637784b64
Nikita Avdosev <avdosev1@yandex.ru>
2022-03-24 22:03:26 +0300

    make some n-grams

--- lab2/predcomputation.py
+++ lab2/predcomputation.py
@@ -1,5 +1,6 @@
-import json
+# for run use pypy3
 
+import json
 from collections import Counter
 
 
@@ -15,9 +16,9 @@ all_words = [word for text in texts for word in text]
 counts = Counter(all_words)
 
 def make_n_gram(text, n_grams):
-    for i in range(len(text)):
+    for i in range(-n_grams+1, len(text)+n_grams-1):
         words = []
-        for j in range(i-n_grams//2, i+n_grams//2+n_grams%2):
+        for j in range(i, i+n_grams):
             if j < 0: words.append(START_SEQ)
             elif j >= len(text): words.append(END_SEQ)
             else: words.append(text[j])
@@ -27,21 +28,31 @@ def make_n_gram(text, n_grams):
 for item in counts.items():
     result[item[0]] = item[1] / len(all_words)
 
-n_gram_counts = Counter()
-for text in texts:
-    n_gram_counts.update(['~'.join(n_gram) for n_gram in make_n_gram(text, 2)])
-counts.update(n_gram_counts)
-counts.update({END_SEQ: len(texts)})
-counts.update({START_SEQ: len(texts)})
+def save_n_gramm(n):
+    print('n-grams', n)
 
-new_result = {}
+    n_gram_counts = Counter()
+    for text in texts:
+        n_gram_counts.update(['~'.join(n_gram) for n_gram in make_n_gram(text, n)])
+    
+    counts.update(n_gram_counts)
+    counts.update({'~'.join([END_SEQ]*(n-1)): len(texts)})
+    counts.update({'~'.join([START_SEQ]*(n-1)): len(texts)})
 
-for n_gram, count in n_gram_counts.items():
-    i = n_gram.find('~')
-    word, u_gram = n_gram[:i], n_gram[i+1:]
-    new_result[n_gram] = count / counts[word]
+    new_result = {}
+    k = 0.001
+    v = sum(n_gram_counts.values())
+    print(f'v={v}')
+    for n_gram, count in n_gram_counts.items():
+        i = n_gram.rfind('~')
+        word, u_gram  = n_gram[:i], n_gram[i+1:]
+        try:
+            new_result[n_gram] = (count + k) / (counts[word] + k*v)
+        except ZeroDivisionError:
+            print(n_gram)
 
-with open('lab2/cache/data.json', 'w', encoding='utf-8') as f:
-    json.dump(new_result, f, ensure_ascii=False, indent=1)
+    with open(f'lab2/cache/data_{n}.json', 'w', encoding='utf-8') as f:
+        json.dump(new_result, f, ensure_ascii=False, indent=1)
 
-# есть смысл сформировать более удобное представление данных
+for n in range(2, 6):
+    save_n_gramm(n)
