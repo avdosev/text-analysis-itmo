2c009ca1a04a6650a7a43385e8c2fca907fee580
Nikita Avdosev <avdosev1@yandex.ru>
2022-04-22 21:15:07 +0300

    in progress

--- lab3/fast_text.py
+++ lab3/fast_text.py
@@ -1,5 +1,13 @@
 from common import *
+import fasttext
+import fasttext.util
 
 words = load_words()
 
-print(words[:10])
+fasttext.util.download_model('ru', if_exists='ignore') 
+ft = fasttext.load_model('cc.ru.300.bin')
+
+wecs = ft.get_word_vector(words)
+
+print('wecs:')
+print(wecs[:10])
